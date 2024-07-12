2487e1673445322bcaa9f350afd3ef90bab7d323
Tomáš Luber <tomas.luber@ugn.cas.cz>
2024-07-12 15:44:58 +0200

    WIP

--- pLaplace_simple.py
+++ pLaplace_simple.py
@@ -15,8 +15,56 @@ from basix.ufl import element
 import pyvista
 
 
+def pLaplace_zero_dirichlet(mesh, p, element_type="Lagrange", order=1):
+    x = ufl.SpatialCoordinate(mesh)
 
-if __name__ == '__main__':
+    P_n = element(element_type, mesh.basix_cell(), order)
+    Q = dfx.fem.functionspace(mesh, P_n)
+    v = ufl.TestFunction(Q)
+    u = dfx.fem.Function(Q)
+
+    # take from tutorial page; marks all boundary entities
+    boundary_facets = dfx.mesh.locate_entities_boundary(mesh, 1, lambda x: np.full(x.shape[1], True, dtype=bool))
+
+    bc = dfx.fem.dirichletbc(Constant(mesh, 0.0),
+                             dfx.fem.locate_dofs_topological(Q, 1, boundary_facets), Q)
+
+    # energy functional for pLaplace
+    E = 1.0 / p * inner(grad(u), grad(u)) ** (p / 2.0) * dx
+    # custom rhs
+    E += - x[0] * u * dx
+
+    F = ufl.derivative(E, u)
+    J = ufl.derivative(F, u)
+
+    u_init_expression = dfx.fem.Expression(x[0] * (1 - x[0]) * (1 - x[1]) * x[1], Q.element.interpolation_points())
+    u.interpolate(u_init_expression)
+
+    problem = NonlinearProblem(F, u, bcs=[bc], J=J)
+
+    solver = NewtonSolver(MPI.COMM_WORLD, problem)
+    solver.convergence_criterion = "incremental"
+    solver.rtol = 1e-6
+    solver.report = True
+    solver.max_it = 200
+
+    ksp = solver.krylov_solver
+    opts = PETSc.Options()
+    option_prefix = ksp.getOptionsPrefix()
+    opts[f"{option_prefix}ksp_type"] = "preonly"
+    opts[f"{option_prefix}pc_type"] = "lu"
+    opts[f"{option_prefix}pc_factor_mat_solver_type"] = "mumps"
+    ksp.setFromOptions()
+
+    log.set_log_level(log.LogLevel.INFO)
+    n, converged = solver.solve(u)
+    assert (converged)
+    print(f"Number of interations: {n:d}")
+    return u
+
+
+
+def test():
     square_size = 1
     mesh_size = 20
     lagrange_element_order = 1
@@ -25,7 +73,6 @@ if __name__ == '__main__':
                                      [[0.0, 0.0], [square_size, square_size]],
                                      [mesh_size, mesh_size],
                                      cell_type=dfx.mesh.CellType.triangle)
-    
 
     with dfx.io.XDMFFile(MPI.COMM_WORLD, 'circle_with_hole.xdmf', 'r') as mesh_file:
         mesh = mesh_file.read_mesh(name='Grid')
@@ -37,7 +84,6 @@ if __name__ == '__main__':
     with dfx.io.XDMFFile(MPI.COMM_WORLD, 'circle_with_hole_boundary.xdmf', 'r') as boundary_file:
         facet_tags = boundary_file.read_meshtags(mesh, name='Grid')
 
-
     x = ufl.SpatialCoordinate(mesh)
 
     P_n = element("Lagrange", mesh.basix_cell(), lagrange_element_order)
@@ -48,16 +94,16 @@ if __name__ == '__main__':
     boundary_facets = dfx.mesh.locate_entities_boundary(mesh, 1, lambda x: np.full(x.shape[1], True, dtype=bool))
     bc = dfx.fem.dirichletbc(Constant(mesh, 0.0),
                              dfx.fem.locate_dofs_topological(Q, 1, boundary_facets), Q)
-    
+
     p = 5
-    E = 1.0/p*inner(grad(u), grad(u))**(p/2.0)*dx 
-    E += - x[0]*u*dx
+    E = 1.0 / p * inner(grad(u), grad(u)) ** (p / 2.0) * dx
+    E += - x[0] * u * dx
     # E += - x[0]*(1-x[0])*(1-x[1])*x[1]*u*dx
     # F = inner(grad(u), grad(v))*dx
     F = ufl.derivative(E, u)
     J = ufl.derivative(F, u)
 
-    u_init_expression = dfx.fem.Expression(x[0]*(1-x[0])*(1-x[1])*x[1], Q.element.interpolation_points())
+    u_init_expression = dfx.fem.Expression(x[0] * (1 - x[0]) * (1 - x[1]) * x[1], Q.element.interpolation_points())
     u.interpolate(u_init_expression)
 
     problem = NonlinearProblem(F, u, bcs=[bc], J=J)
@@ -81,7 +127,7 @@ if __name__ == '__main__':
     assert (converged)
     print(f"Number of interations: {n:d}")
 
-    grid_uh = pyvista.UnstructuredGrid(*dfx.plot.vtk_mesh(Q ))
+    grid_uh = pyvista.UnstructuredGrid(*dfx.plot.vtk_mesh(Q))
     grid_uh.point_data["u"] = u.x.array.real
     grid_uh.set_active_scalars("u")
     p2 = pyvista.Plotter()
@@ -94,3 +140,13 @@ if __name__ == '__main__':
     p3.title = 'Solution in 3d'
     p3.add_mesh(warped, show_edges=True, show_scalar_bar=True, scalar_bar_args={'vertical': True})
     p3.show()
+
+
+
+if __name__ == '__main__':
+    square_size = 1
+    mesh_size = 20
+    mesh = dfx.mesh.create_rectangle(MPI.COMM_WORLD,
+                                     [[0.0, 0.0], [square_size, square_size]],
+                                     [mesh_size, mesh_size],
+                                     cell_type=dfx.mesh.CellType.triangle)
