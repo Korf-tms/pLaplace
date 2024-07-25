2a29d8d5e65faf0c525762b261251149154bb7bb
Tomáš Luber <tomas.luber@ugn.cas.cz>
2024-07-25 13:50:31 +0200

    Finished test example

--- pLaplace_simple.py
+++ pLaplace_simple.py
@@ -31,20 +31,22 @@ def pLaplace_zero_dirichlet(mesh, p, element_type="Lagrange", order=1):
 
     # energy functional for pLaplace
     E = 1.0 / p * inner(grad(u), grad(u)) ** (p / 2.0) * dx
-    # custom rhs
-    E += - x[0] * u * dx
+    # TODO: custom rhs as input parameter?
+    E += - 10*ufl.sin(x[0]*x[1]) * u * dx
 
     F = ufl.derivative(E, u)
     J = ufl.derivative(F, u)
 
-    u_init_expression = dfx.fem.Expression(x[0] * (1 - x[0]) * (1 - x[1]) * x[1], Q.element.interpolation_points())
+    # TODO: what is a good initial guess?
+    # TODO: how to integrate as input to this function?
+    u_init_expression = dfx.fem.Expression(x[0] * (2 - x[0]) * (2 - x[1]) * x[1], Q.element.interpolation_points())
     u.interpolate(u_init_expression)
 
     problem = NonlinearProblem(F, u, bcs=[bc], J=J)
 
     solver = NewtonSolver(MPI.COMM_WORLD, problem)
     solver.convergence_criterion = "incremental"
-    solver.rtol = 1e-6
+    solver.rtol = 1e-7
     solver.report = True
     solver.max_it = 200
 
@@ -58,95 +60,46 @@ def pLaplace_zero_dirichlet(mesh, p, element_type="Lagrange", order=1):
 
     log.set_log_level(log.LogLevel.INFO)
     n, converged = solver.solve(u)
-    assert (converged)
-    print(f"Number of interations: {n:d}")
+    assert converged
     return u
 
 
-
-def test():
-    square_size = 1
-    mesh_size = 20
-    lagrange_element_order = 1
-
-    mesh = dfx.mesh.create_rectangle(MPI.COMM_WORLD,
-                                     [[0.0, 0.0], [square_size, square_size]],
-                                     [mesh_size, mesh_size],
-                                     cell_type=dfx.mesh.CellType.triangle)
-
-    with dfx.io.XDMFFile(MPI.COMM_WORLD, 'circle_with_hole.xdmf', 'r') as mesh_file:
-        mesh = mesh_file.read_mesh(name='Grid')
-        cell_tags = mesh_file.read_meshtags(mesh, name='Grid')
-
-    mesh.topology.create_connectivity(mesh.topology.dim, mesh.topology.dim - 1)  # taken from the link, why is it here?
-
-    # prepare tags for boudary conditions
-    with dfx.io.XDMFFile(MPI.COMM_WORLD, 'circle_with_hole_boundary.xdmf', 'r') as boundary_file:
-        facet_tags = boundary_file.read_meshtags(mesh, name='Grid')
-
-    x = ufl.SpatialCoordinate(mesh)
-
-    P_n = element("Lagrange", mesh.basix_cell(), lagrange_element_order)
-    Q = dfx.fem.functionspace(mesh, P_n)
-    v = ufl.TestFunction(Q)
-    u = dfx.fem.Function(Q)
-
-    boundary_facets = dfx.mesh.locate_entities_boundary(mesh, 1, lambda x: np.full(x.shape[1], True, dtype=bool))
-    bc = dfx.fem.dirichletbc(Constant(mesh, 0.0),
-                             dfx.fem.locate_dofs_topological(Q, 1, boundary_facets), Q)
-
-    p = 5
-    E = 1.0 / p * inner(grad(u), grad(u)) ** (p / 2.0) * dx
-    E += - x[0] * u * dx
-    # E += - x[0]*(1-x[0])*(1-x[1])*x[1]*u*dx
-    # F = inner(grad(u), grad(v))*dx
-    F = ufl.derivative(E, u)
-    J = ufl.derivative(F, u)
-
-    u_init_expression = dfx.fem.Expression(x[0] * (1 - x[0]) * (1 - x[1]) * x[1], Q.element.interpolation_points())
-    u.interpolate(u_init_expression)
-
-    problem = NonlinearProblem(F, u, bcs=[bc], J=J)
-
-    solver = NewtonSolver(MPI.COMM_WORLD, problem)
-    solver.convergence_criterion = "incremental"
-    solver.rtol = 1e-6
-    solver.report = True
-    solver.max_it = 200
-
-    ksp = solver.krylov_solver
-    opts = PETSc.Options()
-    option_prefix = ksp.getOptionsPrefix()
-    opts[f"{option_prefix}ksp_type"] = "preonly"
-    opts[f"{option_prefix}pc_type"] = "lu"
-    opts[f"{option_prefix}pc_factor_mat_solver_type"] = "mumps"
-    ksp.setFromOptions()
-
-    log.set_log_level(log.LogLevel.INFO)
-    n, converged = solver.solve(u)
-    assert (converged)
-    print(f"Number of interations: {n:d}")
-
+def plot_solution(u_function, plot_in_3d=False):
+    Q = u_function.function_space
     grid_uh = pyvista.UnstructuredGrid(*dfx.plot.vtk_mesh(Q))
-    grid_uh.point_data["u"] = u.x.array.real
+    grid_uh.point_data["u"] = u_function.x.array.real
     grid_uh.set_active_scalars("u")
     p2 = pyvista.Plotter()
+    p2.show_bounds()
     p2.title = 'Solution'
     p2.add_mesh(grid_uh, show_edges=True, scalar_bar_args={'vertical': True})
     p2.show()
 
-    warped = grid_uh.warp_by_scalar()
-    p3 = pyvista.Plotter()
-    p3.title = 'Solution in 3d'
-    p3.add_mesh(warped, show_edges=True, show_scalar_bar=True, scalar_bar_args={'vertical': True})
-    p3.show()
-
+    if plot_in_3d:
+        warped = grid_uh.warp_by_scalar()
+        p3 = pyvista.Plotter()
+        p3.show_bounds()
+        p3.title = 'Solution in 3d'
+        p3.add_mesh(warped, show_edges=True, show_scalar_bar=True, scalar_bar_args={'vertical': True})
+        p3.show()
 
 
 if __name__ == '__main__':
-    square_size = 1
-    mesh_size = 20
+    # common p for two meshes
+    p = 4
+    # square mesh
+    square_size = 2
+    mesh_size = 40
     mesh = dfx.mesh.create_rectangle(MPI.COMM_WORLD,
                                      [[0.0, 0.0], [square_size, square_size]],
                                      [mesh_size, mesh_size],
                                      cell_type=dfx.mesh.CellType.triangle)
+    u = pLaplace_zero_dirichlet(mesh, p)
+    plot_solution(u, True)
+
+    # circular mesh generated by gpt_gmsh_mesh.py
+    with dfx.io.XDMFFile(MPI.COMM_WORLD, 'circle_with_hole.xdmf', 'r') as mesh_file:
+        mesh = mesh_file.read_mesh(name='Grid')
+
+    u = pLaplace_zero_dirichlet(mesh, p)
+    plot_solution(u, True)
